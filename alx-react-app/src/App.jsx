679202e67d810246b3f367da66cc09b4ef7c5422
promise2000-oss <promisechuks93@gmail.com>
2026-01-10 09:42:04 +0100

    corection

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
 
   return (
     <>
-      {welcomeMessage()}
+      <welcomeMessage />
+      {welcomeMessage}
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
