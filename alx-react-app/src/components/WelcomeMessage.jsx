bb8c5e50bed4a7fb90d7ca21a2367b363a3169c8
promise2000-oss <promisechuks93@gmail.com>
2026-01-10 09:39:30 +0100

    corrected my mistakes

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -1,9 +1,10 @@
 function welcomeMessage() {
     return (
+       
         <div>
-            <h1>Welcome to ALX react app</h1>
-            <p>This is a simple JSX component.</p>
-        </div>    
+            <h1>Hello everyone, I am learning React at ALX!</h1>
+            <p>I am learning about JSX!</p>
+        </div>
     );
 }
 
