195e43bb980a317361ccb9427072b7b0ebc81dd2
promise2000-oss <promisechuks93@gmail.com>
2026-01-17 20:08:46 +0100

    changed the of the dir

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+function MainContent () {
+
+    return (
+        <main style={{border: "2px solid gray", borderRadius: "8px", padding: "8px"}}>
+            <p style={{color: "black"}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    )
+}
+
+export default MainContent;
