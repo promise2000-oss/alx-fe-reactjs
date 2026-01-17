d71db53c9d16b374386da3ee289b8f7de104ef73
promise2000-oss <promisechuks93@gmail.com>
2026-01-17 13:51:53 +0100

    First Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+
+    return (
+        <footer style={{backgroundColor: "black", borderRadius: "8px", padding: "1rem", marginBlock: "1rem"}}>
+            <p style={{color: "white"}}>© 2023 City Lovers</p>
+        </footer>
+    )
+}
+
+export default Footer;
