f45749508730bafebcc1aa6c9f5929dfc890e24a
promise2000-oss <promiseplanet@gmail.com>
2026-02-03 09:01:23 +0100

    added tailwind-react-integration

---
+++ tailwind-react-integration/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  plugins: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false,
+  theme: { extend: {} },
+  variants: { extend: {} },
+}
