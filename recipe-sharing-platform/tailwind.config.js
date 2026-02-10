e1c53d518a5356f8757754960a15d5c6fb36cc1f
promise2000-oss <promisechuks93@gmail.com>
2026-02-10 10:47:47 +0100

    following alx

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
