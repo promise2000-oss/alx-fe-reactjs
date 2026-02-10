3fb82483586ac3a7602b250041f74aa63a86ce0a
promise2000-oss <promisechuks93@gmail.com>
2026-02-10 10:23:50 +0100

    corrected

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
     extend: {},
   },
-  plugins: ["prettier-plugin-tailwindcss"],
-}
+  plugins: [],
+};
